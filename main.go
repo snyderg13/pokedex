65b0653c4bde3ee64d8579969a8d78b168625de4
snyderg13 <georgesny13@gmail.com>
2025-06-24 20:01:56 -0400

    addition of function to clean and split input text

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
-    fmt.Println("Hello World!")
+func cleanInput(text string) []string {
+	if len(text) == 0 {
+		return []string{}
+	}
+
+	return strings.Fields(strings.ToLower(text))
 }
 
+func main() {
+	fmt.Println("Hello World!")
+	cleanInput("ANOTHER TEST STRING    IN    HERE")
+	cleanInput("My TeST Strings hereE")
+}
