8ebd755949e470b307df811375adb3ece0d9ff2c
snyderg13 <georgesny13@gmail.com>
2025-06-24 20:42:39 -0400

    refactor to use a switch and add initial usage info

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
+func printUsage() {
+	fmt.Println("-----------------------------------------------------")
+	fmt.Println("Just type in a message and we'll echo the first word")
+	fmt.Println("-----------------------------------------------------")
+}
+
 func main() {
 	var line string
 	var words []string
@@ -29,10 +35,14 @@ func main() {
 		}
 		line = inputScanner.Text()
 		words = cleanInput(line)
-		if words[0] == "exit" {
+		switch words[0] {
+		case "exit":
 			fmt.Println("Goodbye!")
 			os.Exit(0)
+		case "help":
+			printUsage()
+		default:
+			fmt.Printf("\nYour command was: %s\n", words[0])
 		}
-		fmt.Printf("\nYour command was: %s\n", words[0])
 	}
 }
