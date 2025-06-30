384c7ebbfadb452d72dd938e9f17d1cb3d222a2b
snyderg13 <georgesny13@gmail.com>
2025-06-30 14:33:43 -0400

    create interface for HTTP GET fetches for APIs - WIP for future generics/IF cleanup

--- internal/pokeapi/pokeClient.go
+++ internal/pokeapi/pokeClient.go
@@ -13,7 +13,7 @@ import (
 const (
 	pokeAPIBaseURL       = "https://pokeapi.co/api/v2/"
 	locationAreaEndpoint = "https://pokeapi.co/api/v2/location-area/"
-	cacheReapRate        = 5 * time.Second
+	cacheReapRate        = 10 * time.Second
 )
 
 var pokeAPIDebug = false
@@ -44,6 +44,66 @@ type LocAreaResp struct {
 	Next    string    `json:"next"`
 	Prev    string    `json:"previous"`
 	Results []locArea `json:"results"`
+}
+
+type PokeFetch interface {
+	DoGetData(string) (any, error)
+}
+
+func (l LocAreaResp) DoGetData(url string) (LocAreaResp, error) {
+	if url == "" {
+		url = locationAreaEndpoint
+	}
+
+	var results LocAreaResp
+	cacheData, found := pokeAPICache.Get(url)
+	if found {
+		err := json.Unmarshal(cacheData, &results)
+		if err != nil {
+			fmt.Println("failed to unmarshal cache data", err)
+		}
+
+		if pokeAPIDebug {
+			fmt.Println("CLIENT: Cache get was used")
+		}
+		return results, err
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		// @TODO cleanup below lines
+		fmt.Println("http req failed")
+		fmt.Println(fmt.Errorf("http req failed: %w", err))
+		return results, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return results, fmt.Errorf("status code (%d) > 299", res.StatusCode)
+	} else if res.StatusCode != 200 {
+		fmt.Printf("status code (%d) != 200\n", res.StatusCode)
+	}
+
+	// convert results to []byte and add to the cache
+	bytesBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return results, err
+	}
+
+	// add data byte slice to cache
+	pokeAPICache.Add(url, bytesBody)
+	if pokeAPIDebug {
+		fmt.Println("CLIENT: Cache add was used")
+	}
+
+	// unmarshal into the LocAreaResp to return to the caller
+	err = json.Unmarshal(bytesBody, &results)
+	if err != nil {
+		fmt.Println("failed to unmarshal cache data", err)
+	}
+
+	return results, nil
 }
 
 func GetLocationAreas(locURL string) (LocAreaResp, error) {
@@ -89,6 +149,115 @@ func GetLocationAreas(locURL string) (LocAreaResp, error) {
 
 	// add data byte slice to cache
 	pokeAPICache.Add(locURL, bytesBody)
+	if pokeAPIDebug {
+		fmt.Println("CLIENT: Cache add was used")
+	}
+
+	// unmarshal into the LocAreaResp to return to the caller
+	err = json.Unmarshal(bytesBody, &results)
+	if err != nil {
+		fmt.Println("failed to unmarshal cache data", err)
+	}
+
+	return results, nil
+}
+
+type PokemonEncounters struct {
+	Pokemon struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"pokemon"`
+	VersionDetails []struct {
+		EncounterDetails []struct {
+			Chance          int   `json:"chance"`
+			ConditionValues []any `json:"condition_values"`
+			MaxLevel        int   `json:"max_level"`
+			Method          struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"method"`
+			MinLevel int `json:"min_level"`
+		} `json:"encounter_details"`
+		MaxChance int `json:"max_chance"`
+		Version   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"version_details"`
+}
+
+type LocationDetails struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonList []PokemonEncounters `json:"pokemon_encounters"`
+}
+
+// @TODO add test cases for different commands
+func (l LocationDetails) DoGetData(locName string) (LocationDetails, error) {
+	url := locationAreaEndpoint + locName + "/"
+	var results LocationDetails
+	cacheData, found := pokeAPICache.Get(url)
+	if found {
+		err := json.Unmarshal(cacheData, &results)
+		if err != nil {
+			fmt.Println("failed to unmarshal cache data", err)
+		}
+
+		if pokeAPIDebug {
+			fmt.Println("CLIENT: Cache get was used")
+		}
+		return results, err
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		// @TODO cleanup below lines
+		fmt.Println("http req failed")
+		fmt.Println(fmt.Errorf("http req failed: %w", err))
+		return results, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return results, fmt.Errorf("status code (%d) > 299", res.StatusCode)
+	} else if res.StatusCode != 200 {
+		fmt.Printf("status code (%d) != 200\n", res.StatusCode)
+	}
+
+	// convert results to []byte and add to the cache
+	bytesBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return results, err
+	}
+
+	// add data byte slice to cache
+	pokeAPICache.Add(url, bytesBody)
 	if pokeAPIDebug {
 		fmt.Println("CLIENT: Cache add was used")
 	}
